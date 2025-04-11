f064fd8f7299ade800b951d24faf80e55af50fe4
Animus <pyramidteam01@gmail.com>
2025-04-11 19:16:52 -0400

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['discord-oauth2'],
-    exclude: ['lucide-react'],
-    esbuildOptions: {
-      target: 'es2020',
-    }
-  },
   build: {
     target: 'es2020',
-    commonjsOptions: {
-      include: [/discord-oauth2/, /node_modules/],
-      transformMixedEsModules: true
-    },
     rollupOptions: {
+      external: ['discord-oauth2'],
       output: {
-        manualChunks: {
-          'discord-oauth2': ['discord-oauth2']
+        globals: {
+          'discord-oauth2': 'DiscordOAuth2'
         }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      'node-fetch': 'isomorphic-fetch'
     }
   }
 });
